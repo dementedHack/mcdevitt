e7cdaf8ef261f02e2fcb0041841738a989285437
dementedHack <kaiperez@yahoo.com>
2017-06-14 07:01:56 -0700

    Reading from DB, some parsing done
    Need to fix issue where page clicks are not being properly parsed.

--- models/city.js
+++ models/city.js
@@ -1,10 +1,10 @@
 module.exports = class City {
-	constructor (name, id, cities, population, surfaceArea, retailLocations) {
+	constructor (name, id, population, surfaceArea, retailLocations, metropolitan) {
     	this.name = name;
     	this.id = id;
-    	this.cities = cities;
     	this.population = population;
     	this.surfaceArea = surfaceArea;
     	this.retailLocations = retailLocations
+    	this.metropolitan = metropolitan
 	}
 }
--- routes/city.js
+++ routes/city.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Metropolitan = require('../models/metropolitan');
+var City = require('../models/city');
 var firebase = require("firebase");
 var cityItems = [];
 
@@ -15,19 +15,37 @@ var cityItems = [];
 var database = firebase.database();
 
 router.get('/', function(req, res, next){
-	readCityItemFromDB();
+	// Parse request parameters sent
+	var fullyParsedRequest = "";
+	var parsedRequestId = req.url.split("=")[1];
+	var parsedArr = parsedRequestId.split("%20")
+	
+	for(var i = 0; i < parsedArr.length; i++) {
+		if (i == parsedArr.length - 1) {
+			fullyParsedRequest += parsedArr[i]
+		} else {
+			fullyParsedRequest += parsedArr[i] + " "
+		}
+	}
+	console.log(fullyParsedRequest)
+	// Send parsed parameter as an input to query DB
+	readCityItemFromDBByMetroName(fullyParsedRequest);
 	setTimeout(function(){
 		res.json(cityItems);
 	}, 200);
-	
 });
 
-function readCityItemFromDB() {
+function readCityItemFromDBByMetroName(metroName) {
+	console.log(metroName);
 	cityItems = [];
-	var ref = firebase.database().ref("city");
+	var ref = firebase.database().ref("city").orderByChild('metropolitan').equalTo(metroName);
 	ref.on("child_added", function(snapshot) {
-		cityItems.push(snapshot.val())
+		var citiesList;
+		var cityItemReadFromDB = new City(snapshot.key, snapshot.val().id, snapshot.val().population, snapshot.val().surfaceArea, snapshot.val().retailLocations, snapshot.val().metropolitan);
+			console.log(cityItemReadFromDB);
+			cityItems.push(cityItemReadFromDB);
 	})
+	
 }
 
 module.exports = router;
